ab2f856245b43824ae37877138aba017cd7cbd3f
BICKY03 <jotirajmajhi29@gmail.com>
2024-05-17 00:34:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
   const [count, setCount] = useState(0);
 
   function increment(){
-    setCount((prev)=>prev+1)
+    setCount(count+1)
   }
 
   function decrement(){
-    setCount((prev)=>prev-1)
+    setCount(count-1)
   }
 
   return (
