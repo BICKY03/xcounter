ee7cb2a190b2e3f4c99ce380942750c92bcb5be0
BICKY03 <jotirajmajhi29@gmail.com>
2024-05-17 00:42:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import { useState } from 'react';
 
 function App() {
-
   const [count, setCount] = useState(0);
 
-  function increment(){
-    setCount(count+1)
-  }
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  };
 
-  function decrement(){
-    setCount(count-1)
-  }
+  const decrement = () => {
+    setCount((prev) => prev - 1);
+  };
 
   return (
     <div className="App">
       <h1>Counter App</h1>
-      <p>count:{count}</p>
+      <p>Count: {count}</p>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
-     </div>
+    </div>
   );
 }
 
